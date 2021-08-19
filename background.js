a88b863752d02d0161ced430e8ecc4e5dccd71d1
Alex Waters <AlexanderWatermenAWC@gmail.com>
2021-08-19 17:32:35 -0400

    almost

--- background.js
+++ background.js
@@ -1,8 +1,41 @@
 let color = '#3aa757';
 
 function postback() {
-  // alert('test123');
-  javascript:document.getElementById("btnRefresh").click();
+  
+  // https://stackoverflow.com/a/10126042
+  var inactivityTime = function () {
+    console.log("starting timer")
+    
+    var time;
+    
+    window.onload = resetTimer;    
+    // DOM Events
+    // document.onmousemove = resetTimer;
+    document.onmousemove = resetTimer;
+    document.onkeydown = resetTimer;
+    
+    var reload_timer;
+
+    function idle() {
+        console.log("You are now idle ")
+        reload_timer = window.setInterval(function(){
+          console.log("Refreshing")
+          RightFrame.location.href = RightFrame.location.href;
+        }, 5000); // how many mill secs till refresh
+    }
+
+    function resetTimer() {
+        clearTimeout(time);
+        console.log("You are now no longer idle ")
+        time = setTimeout(idle, 10000) // how many mill secs till start idleing
+        clearInterval(reload_timer);
+    }
+  };
+
+  resetTimer();
+  inactivityTime();
+
+
 }
 
 
@@ -11,27 +44,34 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
 
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener( function (tabId_main, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       var tabURL = tabs[0].url;
-      var tabID = tabs[0].id
-      console.log(tabID);
+      var tabId = tabs[0].id
+      console.log(tabId);
 
       if (tabURL == "https://support.gmhec.org/TDNext/Home/Desktop/Default.aspx"){
-        setTimeout(() => {  
+        setTimeout(() => {
 
-          console.log("REFRESHING!");
+          let IFR_URL = 'https://support.gmhec.org/TDNext/Apps/51/Tickets/Default.aspx'
+          chrome.webNavigation.getAllFrames({tabId},(vals) => {
+            
+            console.log(vals)
+            var frame = vals.find((e) => e.url == IFR_URL && e.parentFrameId == 0) 
+            console.log("FRAME: ", frame)
+            
+            chrome.scripting.executeScript(
+              {
+                target: {tabId: tabId, frameIds: [frame.frameId]},
+                func: postback,
+              });
+
+          });
+
+          }, 10000); // time to navigate to correct page
           
-          chrome.scripting.executeScript(
-            {
-              target: {tabId: tabID},
-              func: postback,
-            });
-
-        }, 10000);
-        
       }
   });
 
